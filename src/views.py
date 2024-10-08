94df4dff5c25b192c6470b758e25b23e689fe0e3
Angelina-hub <130393386+Angelina-hub@users.noreply.github.com>
2024-10-08 18:43:44 +0300

    Create views.py

---
+++ src/views.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def get_rectangle_area(width, height):
+    return width * height
+
+def get_square_area(side):
+    return side ** 2
+
+def get_circle_area(radius):
+    return pi * radius ** 2
